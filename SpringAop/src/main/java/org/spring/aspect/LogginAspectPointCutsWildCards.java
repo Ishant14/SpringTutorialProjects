e02a3981220b854ff9876c2166cacebc940de0da
Ishant Gaurav <igaurav@XEBIAINDIA.LOCAL>
2017-01-22 14:24:50 +0530

    Point cut added

--- SpringAop/src/main/java/org/spring/aspect/LogginAspectPointCutsWildCards.java
+++ SpringAop/src/main/java/org/spring/aspect/LogginAspectPointCutsWildCards.java
@@ -14,7 +14,7 @@ public class LogginAspectPointCutsWildCards {
     /*if we to run on any type whether public or private ,irrespective of return type,
        any type get method like getName, get getTriangle etc then user = @Before("execution(* get*())")  */
 
-    //if we want to run advice on any get method whether it takes argument or not then we will write as =  get*(..)
+    /*if we want to run advice on any get method whether it takes argument or not then we will write as =  get*(..) */
 
     /* if we want to run the get method but of particular package then we will write as =
        @Before("execution(* packageName.className.get*())")   */
